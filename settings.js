438f654ef585e3709225b0df4166dfb471bda78d
CHERNOBYL124 <126145085+CHERNOBYL124@users.noreply.github.com>
2023-07-22 14:00:23 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'Chernobylzz'// yourname
+global.MONGO_DB_URI = "mongodb+srv://Chernobyl:<password>@cluster0.fkcj4a6.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "yaalokbg@gmail.com" //email
+global.email_password = "zcsgeznpjvvattuy" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
